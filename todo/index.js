6a1cfbb57da7dd768e1931e58ff524c37d9d93f2
Tomas Roy <tomas.roy@ibm.com>
2021-04-06 16:24:15 -0400

    todo example complete

--- todo/index.js
+++ todo/index.js
@@ -1,23 +1,61 @@
 const Express = require("express")
-const { MongoClient } = require("mongodb")
+const BodyParser = require('body-parser');
+const fs = require("fs")
+const { MongoClient, ObjectID } = require("mongodb");
 
+// Setup Express server
 const server = Express()
+const port = process.env.PORT || 8080
 
-let database;
+// Setup database client if credentials exist
+let mongoURI, mongoDatabaseName, mongoClient, db
+if (process.env.DATABASES_FOR_MONGODB_CONNECTION) {
+    let mongoCreds = JSON.parse(process.env.DATABASES_FOR_MONGODB_CONNECTION).mongodb
+
+    // Write cert to filesystem
+    fs.writeFileSync(mongoCreds.certificate.name, mongoCreds.certificate.certificate_base64, { encoding: "base64" })
+
+    // Get needed variables
+    mongoDatabaseName = mongoCreds.database
+    mongoURI = mongoCreds.composed[0]
+    
+    // Create new MongoDB client
+    mongoClient = new MongoClient(mongoURI, {
+        tls: true,
+        tlsCAFile: mongoCreds.certificate.name,
+        useUnifiedTopology: true,
+    })
+}
 
 // Setup backend
+server.use(BodyParser.json())
 server.post("/todo", (req, res) => {
-    if (!database) return res.status(500).send("No database found")
+    if (!db) return res.status(500).send("No database found")
+    let todo = {
+        title: req.body.title,
+        complete: false,
+    }
+    db.collection("todos").insertOne(todo).then(result => {
+        res.status(200).send()
+    }).catch(err => {
+        res.status(500).send(err)
+    })
 })
-server.put("/todo", (req, res) => {
-    if (!database) return res.status(500).send("No database found")
+server.patch("/todo", (req, res) => {
+    if (!db) return res.status(500).send("No database found")
+    db.collection("todos").updateOne({"_id": ObjectID(req.body.id)}, {"$set": {complete: req.body.complete}}).then(result => {
+        res.status(200).send()
+    }).catch(err => {
+        console.log(err)
+        res.status(500).send(err)
+    })
 })
 server.get("/todos", (req, res) => {
-    if (!database) return res.status(500).send("No database found")
-    res.status(200).json({
-        todos: [
-            { id: '12345', title: 'Some todo', complete: true },
-        ],
+    if (!db) return res.status(500).send("No database found")
+    db.collection("todos").find().toArray().then(todos => {
+        res.status(200).json({ todos })
+    }).catch(err => {
+        res.status(500).send(err)
     })
 })
 
@@ -25,5 +63,12 @@ server.get("/todos", (req, res) => {
 server.use(Express.static('public'))
 server.get('/', (req, res) => res.sendFile('./public/index.html'))
 
-const port = process.env.PORT || 8080
+if (mongoClient) {
+    console.log(`Connecting to MongoDB at ${mongoURI}...`)
+    mongoClient.connect(function(err) {
+        if (err) return console.error(err)
+        console.log("Connected to database!");
+        db = mongoClient.db(mongoDatabaseName);
+    });
+}
 server.listen(port, () => console.log(`Express listening on ${port}...`))
--- todo/public/app.js
+++ todo/public/app.js
@@ -1,44 +1,37 @@
 
 $(document).ready(() => {
     getTodos()
-
-    $("#todos").find("checkbox").change(() => {
-        editTodo(this)
-    })
     $("form").submit((event) => {
         event.preventDefault()
         createTodo($("#newTodo").val())
+        $("#newTodo").val("")
     })
 })
 
 function getTodos() {
-    axios.get("/todos")
+    return axios.get("/todos")
         .then((response) => {
             let html = "";
+            if (response.data.todos.length < 1) return $("#todos").html("No todos")
             for (let todo of response.data.todos) {
-                html += `<div><input type="checkbox" id="${todo.id}" ${todo.complete ? 'checked' : null}/>${todo.title}</div>`
+                html += `<div><input type="checkbox" id="${todo._id}" ${todo.complete ? 'checked' : null}/>${todo.title}</div>`
             }
             $("#todos").html(html)
+            $("#todos").find("input").change((event) => {
+                editTodo(event.currentTarget)
+            })
         })
-        .catch((err) => {
-            $("#todos").html(err.response.data)
-        })
+        .catch(err => $("#todos").html(err.response.data))
 }
 
-function editTodo(todoElement) {
-    axios.put("/todo", { id: todoElement.attr("id"), complete: todoElement.checked })
-        .then(() => {
-            getTodos()
-        })
-        .catch((err) => {
-            $("#todos").html(err.response.data)
-        })
+function editTodo(element) {
+    return axios.patch("/todo", { id: element.id, complete: element.checked })
+        .then(() => getTodos())
+        .catch(err => $("#todos").html(err.response.data))
 }
 
 function createTodo(title) {
-    axios.post("/todo", { title })
+    return axios.post("/todo", { title })
         .then(() => getTodos())
-        .catch((err) => {
-            $("#todos").html(err.response.data)
-        })
+        .catch(err => $("#todos").html(err.response.data))
 }
